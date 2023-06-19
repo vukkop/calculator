88cff57b738110238a6f7e9a6b69d153286c2184
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2023-06-19 16:08:13 -0700

    MVP Calculator

--- script.js
+++ script.js
@@ -1,2 +1,52 @@
 var displayDiv = document.querySelector("#display");
-displayDiv.innerText = "Some new value";
+displayDiv.innerText = "0";
+var currentNum = ""
+var firstNum;
+var secondNum;
+var operator;
+
+function press(number) {
+  if (displayDiv.innerText === "0") {
+    currentNum = number
+    displayDiv.innerText = currentNum
+  }
+  else {
+    currentNum += `${number}`
+    displayDiv.innerText = currentNum
+  }
+}
+
+function setOP(op) {
+  firstNum = displayDiv.innerText
+  operator = op
+  clr()
+}
+
+function calculate() {
+  secondNum = displayDiv.innerText
+  firstNum = parseFloat(firstNum)
+  secondNum = parseFloat(secondNum)
+  var res = 0
+  switch (operator) {
+    case "+":
+      res = firstNum + secondNum
+      break;
+    case "-":
+      res = firstNum - secondNum
+      break;
+    case "*":
+      res = firstNum * secondNum
+      break;
+    case "/":
+      res = firstNum / secondNum
+      break;
+    default:
+    break;
+  }
+  res.toString.length
+  displayDiv.innerText = res
+}
+
+function clr() {
+  displayDiv.innerText = "0"
+}
